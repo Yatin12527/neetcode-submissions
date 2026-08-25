d46a0181c403659b43991db97f1e893ad40fae42
yatinder <134225415+Yatin12527@users.noreply.github.com>
2026-08-25 17:18:54 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.size()) {
+            if (prices[left] < prices[right]) {
+                maxProfit = max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;  // reset buy day
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+};
